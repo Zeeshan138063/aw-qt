3b46e0be1b51d39fea804e96b9a29673f41c6d10
zeeshan <zeeshan.muhammad@31g.co.uk>
2026-07-16 15:04:11 +0500

    fix(pcd): use native OS notifications for VPN state changes (thread-safe)

--- aw_qt/main.py
+++ aw_qt/main.py
@@ -118,7 +118,7 @@ def main(
         from . import trayicon  # pylint: disable=import-outside-toplevel
 
         # run the trayicon, wait for signal to quit
-        error_code = trayicon.run(manager, testing=testing, port=config.port, vpn_monitor=vpn_monitor)
+        error_code = trayicon.run(manager, testing=testing, port=config.port)
     elif interactive_cli:
         # just an experiment, don't really see the use right now
         _interactive_cli(manager)
